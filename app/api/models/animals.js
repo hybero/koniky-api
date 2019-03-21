0e12fbc94afe58dc3bde6d6efcd1c76e0c47e007
Pavol Havlik <pavol.havlik@gmail.com>
2019-03-21 09:26:45 +0100

    animals model update

--- app/api/models/animals.js
+++ app/api/models/animals.js
@@ -14,15 +14,25 @@ const animalSchema = new Schema({
         trim: true,
         required: true,
     },
-    type: {
+    animal_type: {
         type: String,
         trim: true,
         required: true,
     },
     age: {
+        type: Number,
+        trim: true,
+        required: true,
+    },
+    gender: {
         type: String,
         trim: true,
         required: true,
+    },
+    color: {
+        type: String,
+        trim: true,
+        required: false,
     },
     notes: {
         type: String,

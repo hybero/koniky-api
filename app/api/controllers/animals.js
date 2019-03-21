57dba59ad991bc615be992f6af761d60b6e2cad8
Pavol Havlik <pavol.havlik@gmail.com>
2019-03-21 09:53:47 +0100

    fix: animals controller

--- app/api/controllers/animals.js
+++ app/api/controllers/animals.js
@@ -20,24 +20,24 @@ module.exports = {
     getAll: function(req, res, next) {
         let animalsList = [];
         //console.log(req);
-        //if (typeof req.params.animalId !== 'undefined') {
-            animalModel.find({}, function(err, animals){
+        if (typeof req.params.userId !== 'undefined') {
+            animalModel.find({user_id: req.params.userId}, function(err, animals){
                 if(err) {
                     next(err);
                 } else {
                     for(let animal of animals) {
-                        animalsList.push({id: animal._id, name: animal.name, released_on: animal.released_on});
+                        animalsList.push({id: animal._id, name: animal.name, animal_type: animal.animal_type, age: animal.age, gender: animal.gender, color: animal.color});
                     }
                     res.json({status: "success", message: "animals list found", data: {animals: animalsList}});
                 }
             });
-        //} else {
-        //    res.json({status:"error", message: "animal_id is required parameter", data:null});
-        //}
+        } else {
+            res.json({status:"error", message: "animal_id is required parameter", data:null});
+        }
     },
 
     updateById: function(req, res, next) {
-        animalModel.findByIdAndUpdate(req.params.animalId, {name:req.body.name}, function(err, animalInfo){
+        animalModel.findByIdAndUpdate(req.params.animalId, {name: req.body.name, animal_type: req.body.animal_type, age: req.body.age, gender: req.body.gender, color: req.body.color}, function(err, animalInfo){
             if(err) {
                 next(err);
             } else {
@@ -58,7 +58,7 @@ module.exports = {
 
     create: function(req, res, next) {
         if (typeof req.body.type !== 'undefined' && typeof req.body.capacity !== 'undefined' && typeof req.body.status !== 'undefined') {
-            animalModel.create({name: req.body.name, released_on: req.body.released_on }, function (err, result) {
+            animalModel.create({name: req.body.name, animal_type: req.body.animal_type, gender: req.body.gender, age: req.body.age, color: req.body.color }, function (err, result) {
                 if(err) {
                     next(err);
                 } else {

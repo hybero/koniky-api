c0686f7beed8c2e976bdb181ce00902666bd36cf
Pavol Havlik <pavol.havlik@gmail.com>
2019-03-21 10:53:03 +0100

    enabling healths

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const logger = require('morgan');
 const users = require('./routes/users');
 const animals = require('./routes/animals');
 const animalEvents = require('./routes/animal-events');
+const healths = require('./routes/healths');
 const bodyParser = require('body-parser');
 const mongoose = require('./config/mongodb'); //database configuration
 var jwt = require('jsonwebtoken');
@@ -27,6 +28,8 @@ app.use('/users', users);
 app.use('/animals', validateUser, animals);
 
 app.use('/animalevents', validateUser, animalEvents);
+
+app.use('/healths', validateUser, healths);
 
 app.get('/favicon.ico', function(req, res) {
     res.sendStatus(204);
